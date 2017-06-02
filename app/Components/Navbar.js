354ab44b97ebf270de76c0443d764498bc46ff91
Devon Bull <dbull@live.com>
2017-06-02 00:14:21 -0600

    navbar now has cool dropdown menu

--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-export const Navbar = () => {
+export const Navbar = (props) => {
+  const favorites = (login) => {
+    if (!login.loginUser.name) {
+      return
+    }
+    return (
+      <li><NavLink to='/Favorites'>Favorites</NavLink></li>
+    )
+  }
   return (
-    <section>
-      <NavLink to='/'>Home</NavLink>
-      <NavLink to='/Favorites'>Favorites</NavLink>
-      <NavLink to='/Login'>Login</NavLink>
-      <NavLink to='/CreateAccount'>CreateAccount</NavLink>
-    </section>
+      <nav className='nav-menu'>
+        <ul className='clearfix'>
+          <li><NavLink to='/'>Home</NavLink></li>
+          <ul className='sub-menu'>
+            {favorites(props)}
+            <li><NavLink to='/Login'>Login</NavLink></li>
+            <li><NavLink to='/CreateAccount'>CreateAccount</NavLink></li>
+          </ul>
+        </ul>
+      </nav>
   )
 }

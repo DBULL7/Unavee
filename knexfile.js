a2e39931f11bba1cc17c361bd758e5080dc5d7b7
Devon Bull <dbull@live.com>
2018-02-03 18:31:20 -0800

    file path changes

--- knexfile.js
+++ knexfile.js
@@ -34,10 +34,10 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
-      directory: 'db/migrations'
+      directory: './db/migrations'
     },
     seeds: {
-      directory: 'db/seeds/dev'
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   }

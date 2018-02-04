b68f6938584339d47e17f4436d28d6eb77c0435d
Devon Bull <dbull@live.com>
2018-02-03 18:14:53 -0800

    production db connection

--- knexfile.js
+++ knexfile.js
@@ -31,19 +31,15 @@ module.exports = {
   // },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: 'db/migrations'
+    },
+    seeds: {
+      directory: 'db/seeds/dev'
+    },
+    useNullAsDefault: true
   }
 
 };
